734d234214f5135e820c41b25cb0058a913fe655
Rob Taylor <rob.taylor@chipflow.io>
2025-04-30 10:02:43 +0100

    Move json-compare to top level

--- tools/json_compare.py
+++ tools/json_compare.py
@@ -1,10 +1,20 @@
 import json
+import os
 import sys
+from pathlib import Path
+
+working_dir = Path(os.environ["PDM_RUN_CWD"] if "PDM_RUN_CWD" in os.environ else "./")
+
+gold_path = Path(sys.argv[1])
+gate_path = Path(sys.argv[2])
+
+gold_path = gold_path if gold_path.is_absolute() else working_dir / gold_path
+gate_path = gate_path if gate_path.is_absolute() else working_dir / gate_path
 
 def main():
-    with open(sys.argv[1], "r") as f:
+    with open(gold_path, "r") as f:
         gold = json.load(f)
-    with open(sys.argv[2], "r") as f:
+    with open(gate_path, "r") as f:
         gate = json.load(f)
     assert len(gold["events"]) == len(gate["events"]), f"mismatch: {len(gold['events'])} events in reference, {len(gate['events'])} in test output"
     for ev_gold, ev_gate in zip(gold["events"], gate["events"]):
