d455096416fa5b8f57a6a8bf99191be353896b73
Rob Taylor <rob.taylor@chipflow.io>
2025-05-01 14:42:37 +0100

    Add logging for json_compare when in CI

--- tools/json_compare.py
+++ tools/json_compare.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 from pathlib import Path
+from pprint import pformat
 
 working_dir = Path(os.environ["PDM_RUN_CWD"] if "PDM_RUN_CWD" in os.environ else "./")
 
@@ -11,6 +12,11 @@ gate_path = Path(sys.argv[2])
 gold_path = gold_path if gold_path.is_absolute() else working_dir / gold_path
 gate_path = gate_path if gate_path.is_absolute() else working_dir / gate_path
 
+def on_ci():
+    if "CI" in os.environ and os.environ["CI"]:
+        return True
+    return False
+
 def main():
     with open(gold_path, "r") as f:
         gold = json.load(f)
@@ -18,6 +24,9 @@ def main():
         gate = json.load(f)
     if len(gold["events"]) != len(gate["events"]):
         print(f"Failed! Event mismatch: {len(gold['events'])} events in reference, {len(gate['events'])} in test output")
+        if on_ci():
+            print(f"Test Output:\n{pformat(gate)}\n")
+            print(f"Reference events:\n{pformat(gold)}\n")
         return 1
     for ev_gold, ev_gate in zip(gold["events"], gate["events"]):
         if ev_gold["peripheral"] != ev_gate["peripheral"] or \
