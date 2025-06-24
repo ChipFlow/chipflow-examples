a1807ad7c29f5783c9a856ae8b774d9fcdab3a88
Rob Taylor <rob.taylor@chipflow.io>
2025-06-24 16:29:06 +0100

    Move common sim infra out to chipflow-lib for mcu_soc

--- minimal/design/sim/doit_build.py
+++ minimal/design/sim/doit_build.py
@@ -1,44 +1,41 @@
 import os
 import sys
-import importlib.resources
 
+VARIABLES = {
+    "OUTPUT_DIR": "./build/sim",
+    "ZIG_CXX": f"{sys.executable} -m ziglang c++",
+    "CXXFLAGS": "-O3 -g -std=c++17 -Wno-array-bounds -Wno-shift-count-overflow -fbracket-depth=1024",
+    "INCLUDES": "-I {OUTPUT_DIR} -I {COMMON_DIR} -I {COMMON_DIR}/vendor -I {RUNTIME_DIR}",
+}
+DOIT_CONFIG = {'action_string_formatting': 'both'}
 
-OUTPUT_DIR  = "./build/sim"
-COMMON_DIR  = importlib.resources.files("common") / "sim"
-SOURCE_DIR  = importlib.resources.files("minimal") / "design/sim"
-RUNTIME_DIR = importlib.resources.files("yowasp_yosys") / "share/include/backends/cxxrtl/runtime"
-
-ZIG_CXX  = f"{sys.executable} -m ziglang c++"
-CXXFLAGS = "-O3 -g -std=c++17 -Wno-array-bounds -Wno-shift-count-overflow -fbracket-depth=1024"
-INCLUDES = f"-I {OUTPUT_DIR} -I {COMMON_DIR} -I {COMMON_DIR}/vendor -I {RUNTIME_DIR}"
-
-
-def task_build_sim_cxxrtl():
-    return {
-        "actions": [f"cd {OUTPUT_DIR} && pdm run yowasp-yosys sim_soc.ys"],
-        "targets": [f"{OUTPUT_DIR}/sim_soc.cc", f"{OUTPUT_DIR}/sim_soc.h"],
-        "file_dep": [f"{OUTPUT_DIR}/sim_soc.ys", f"{OUTPUT_DIR}/sim_soc.il"],
-    }
-
-
-def task_build_sim():
-    exe = ".exe" if os.name == "nt" else ""
-
-    return {
+BUILD_SIM = {
+        "name": "build_sim",
         "actions": [
-            f"{ZIG_CXX} {CXXFLAGS} {INCLUDES} -o {OUTPUT_DIR}/sim_soc{exe} "
-            f"{OUTPUT_DIR}/sim_soc.cc {SOURCE_DIR}/main.cc {COMMON_DIR}/models.cc"
-        ],
+            "{ZIG_CXX} {CXXFLAGS} {INCLUDES} -o {OUTPUT_DIR}/sim_soc{EXE} "
+            "{OUTPUT_DIR}/sim_soc.cc {SOURCE_DIR}/main.cc {COMMON_DIR}/models.cc"
+            ],
         "targets": [
-            f"{OUTPUT_DIR}/sim_soc{exe}"
+            "{OUTPUT_DIR}/sim_soc{EXE}"
         ],
         "file_dep": [
-            f"{OUTPUT_DIR}/sim_soc.cc",
-            f"{OUTPUT_DIR}/sim_soc.h",
-            f"{SOURCE_DIR}/main.cc",
-            f"{COMMON_DIR}/models.cc",
-            f"{COMMON_DIR}/models.h",
-            f"{COMMON_DIR}/vendor/nlohmann/json.hpp",
-            f"{COMMON_DIR}/vendor/cxxrtl/cxxrtl_server.h",
+            "{OUTPUT_DIR}/sim_soc.cc",
+            "{OUTPUT_DIR}/sim_soc.h",
+            "{SOURCE_DIR}/main.cc",
+            "{COMMON_DIR}/models.cc",
+            "{COMMON_DIR}/models.h",
+            "{COMMON_DIR}/vendor/nlohmann/json.hpp",
+            "{COMMON_DIR}/vendor/cxxrtl/cxxrtl_server.h",
         ],
     }
+
+SIM_CXXRTL = {
+        "name": "sim_cxxrtl",
+        "actions": ["cd {OUTPUT_DIR} && pdm run yowasp-yosys sim_soc.ys"],
+        "targets": ["{OUTPUT_DIR}/sim_soc.cc", "{OUTPUT_DIR}/sim_soc.h"],
+        "file_dep": ["{OUTPUT_DIR}/sim_soc.ys", "{OUTPUT_DIR}/sim_soc.il"],
+    }
+
+TASKS = [BUILD_SIM, SIM_CXXRTL]
+
+
